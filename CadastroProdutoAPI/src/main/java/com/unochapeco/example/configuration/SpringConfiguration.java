da6ac6e569f8d51233c6b53b1bd6028d914b191a
Luan Felipe Knebel <luan_knebel@hotmail.com>
2023-06-02 21:54:47 -0300

    ajuste

--- CadastroProdutoAPI/src/main/java/com/unochapeco/example/configuration/SpringConfiguration.java
+++ CadastroProdutoAPI/src/main/java/com/unochapeco/example/configuration/SpringConfiguration.java
@@ -31,8 +31,7 @@ public class SpringConfiguration {
 	}
 
 	@Bean
-	public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
-			throws Exception {
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
 		return authenticationConfiguration.getAuthenticationManager();
 	}
 
