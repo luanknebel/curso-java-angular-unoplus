dd8af79a11399c2358a738d9e24248003df0d47b
Luan Felipe Knebel <luan_knebel@hotmail.com>
2023-06-10 17:53:40 -0300

    ajustes

--- CadastroProdutoWeb/src/app/core/security/auth-guard.service.ts
+++ CadastroProdutoWeb/src/app/core/security/auth-guard.service.ts
@@ -15,16 +15,13 @@ export class AuthGuardService implements CanActivate{
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
 
-    let loggedInTrue = new BehaviorSubject<boolean>(true);
-    let loggedInFalse = new BehaviorSubject<boolean>(false);
-
     if (!this.tokenJwtService.isAuthenticated()) {
       this.router.navigate(['/']);
       this.tokenJwtService.removeToken();
       this.notifierService.notifyWarning("Usuario não antenticado para acessar este recurso");
-      return loggedInFalse.asObservable();
+      return false;
     }
 
-    return loggedInTrue.asObservable();
+    return true;
   }
 }
